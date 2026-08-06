d142f42810fed7784a9486beea701e6c48fbc8cc
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-08-06 18:35:27 +0800

    Fix the error when the user is  currently logined and another one makes a new account

--- Chat-App/src/components/Contact/SuggestedFriends.jsx
+++ Chat-App/src/components/Contact/SuggestedFriends.jsx
@@ -106,12 +106,12 @@ export default function SuggestedFriends(){
 
 						return(
 							<div key={friend?.uid} className= 'flex gap-2.5 items-center grow h-12.5'>
-								<div className="image flex justify-center items-center text-white font-semibold w-10 h-10 rounded-full" style={{backgroundColor: friend?.avatarColor}}>
-									{friend?.firstName[0].toUpperCase()}
+								<div className="image flex justify-center items-center text-white font-semibold w-10 h-10 rounded-full" style={{backgroundColor: friend?.avatarColor || "#4B5563"}}>
+									{friend?.firstName?.[0]?.toUpperCase() || "?"}
 								</div>
 								<div className="flex flex-col justify-center font-roboto grow">
-									<p className="font-bold text-[14px] text-white">{`${friend?.firstName} ${friend?.lastName}`}</p>
-									<p className="text-[13px] text-gray-200 mb-1">{friend?.email}</p>
+									<p className="font-bold text-[14px] text-white">{`${friend?.firstName || "User"} ${friend?.lastName || ""}`}</p>
+									<p className="text-[13px] text-gray-200 mb-1">{friend?.email || ""}</p>
 								</div>
 								<button onClick={() => handleToggleFriendRequest(friend?.uid)} 
 								disabled={isLoading}
