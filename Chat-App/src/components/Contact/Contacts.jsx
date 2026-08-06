f45c21d01650a905319762b50b3eaab7af8e00a4
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-08-06 15:30:18 +0800

    make the add friend button interactive

--- Chat-App/src/components/Contact/Contacts.jsx
+++ Chat-App/src/components/Contact/Contacts.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { ref, set, update, onValue, getDatabase } from "firebase/database";
+import app from "../../firebase/firebase.config";
 
 import addFriend from '../../assets/add-friend-gray.png'
 import search from '../../assets/search.png'
@@ -6,9 +8,74 @@ import noChats from '../../assets/conversation.png'
 
 import SuggestedFriends from '../Contact/SuggestedFriends'
 
+function Request(){
+	const [getfriendsRequests, setGetFriendRequests] = useState([]);
+	const db = getDatabase(app);
+
+	return(
+		<>
+			{/* <div className="flex flex-col items-center gap-1">
+				<h2 className="text-[24px] font-roboto font-semibold mt-1 text-black dark:text-white">
+					Friend Requests
+				</h2>
+				<p className="text-sm font-roboto text-black dark:text-zinc-400 max-w-75 text-center">
+					You have no pending requests.
+				</p>
+			</div> */}
+
+			<div className="flex flex-col justify-center w-full gap-y-3 mt-2 overflow-scroll scrollbar-none">
+					<div className="text-black dark:text-white font-semibold text-[16.5px]">
+						<p>Frind Requests (3)</p>
+					</div>
+							<div className= 'flex gap-2.5 items-center grow h-12.5'>
+								<div className="image flex justify-center items-center text-white font-semibold w-10 h-10 rounded-full">
+									M
+								</div>
+								<div className="flex flex-col justify-center font-roboto grow">
+									<p className="font-bold text-[14px] text-white">Marjea Sacdalan</p>
+									<p className="text-[13px] text-gray-200 mb-1">Jea@gmail.com</p>
+								</div>
+								<button className="flex justify-center items-center px-3.5 py-2 gap-x-2 bg-zinc-100 rounded-lg  cursor-pointer">
+									<p className="text-[14px] font-roboto font-semibold text-white dark:text-zinc-900">Accept</p>
+								</button>
+								<button className="flex justify-center items-center px-3.5 py-2 gap-x-2 bg-zinc-800 rounded-lg border border-zinc-300  cursor-pointer">
+									<p className="text-[14px] font-roboto font-semibold text-black dark:text-zinc-200">Decline</p>
+								</button>
+							</div>
+				</div>
+		</>
+	);
+}
+
+export function All(){
+	return(
+		<>
+			<div className='flex flex-col items-center gap-1 mt-13'>
+				{/* <div className='flex justify-center items-center'><img src={noChats} alt="No Conversation" /></div> */}
+				<h2 className="text-[24px] font-roboto font-semibold mt-1 text-black dark:text-white">No friends yet</h2>
+				<p className="text-sm font-roboto text-black dark:text-zinc-400 max-w-75 text-center">Add friends and start connecting with people.</p>
+			</div>
+		</>
+	);
+}
 
 export default function Contacts(){
 	const [filter, setFilter] = useState('all');
+
+	const renderContent = () => {
+    switch (filter) {
+      case 'request':
+        return <Request />;
+      case 'online':
+        return <p className="text-zinc-400 mt-13">No online friends.</p>;
+      case 'offline':
+        return <p className="text-zinc-400 mt-13">No offline friends.</p>;
+      case 'all':
+      default:
+        return <All />;
+    }
+  };
+
 	return(
 		<>
 			<div className='h-full'>
@@ -30,7 +97,7 @@ export default function Contacts(){
 				</header>
 
 				{/* FILTERS */}
-				<div className='w-full flex gap-y-2 items-center mt-5 font-poppins'>
+				{/* <div className='w-full flex gap-y-2 items-center mt-5 font-poppins'>
 					<button 
 					onClick={() => setFilter('all')}
 					className={ `text-black text-[17px] font-semibold px-5 py-2.5 cursor-pointer ${filter === 'all' ? `border-b border-white dark:text-white` : 'dark:text-zinc-400 hover:text-white'}`}>
@@ -43,7 +110,7 @@ export default function Contacts(){
 					</button>
 					<button 
 					onClick={() => setFilter('offline')}
-					className={ `text-blacktext-[17px] font-semibold px-5 py-2.5 cursor-pointer ${filter === 'offline' ? `border-b border-white dark:text-white` : 'dark:text-zinc-400 hover:text-white'}`}>
+					className={ `text-black text-[17px] font-semibold px-5 py-2.5 cursor-pointer ${filter === 'offline' ? `border-b border-white dark:text-white` : 'dark:text-zinc-400 hover:text-white'}`}>
 						Offline
 					</button>
 					<button 
@@ -51,15 +118,23 @@ export default function Contacts(){
 					className={ `text-black text-[17px] font-semibold px-5 py-2.5 cursor-pointer ${filter === 'request' ? `border-b border-white dark:text-white` : 'dark:text-zinc-400 hover:text-white'}`}>
 						Request
 					</button>
-				</div>
+				</div> */}
 
-				<main className='flex flex-col mt-15'>
+				<div className="w-full flex gap-y-2 items-center mt-5 font-poppins">
+        {['all', 'online', 'offline', 'request'].map((tab) => (
+          <button
+            key={tab}
+            onClick={() => setFilter(tab)}
+            className={`capitalize text-[17px] font-semibold px-5 py-2.5 cursor-pointer ${ filter === tab ? 'border-b border-white text-black dark:text-white': 'text-black dark:text-zinc-400 hover:text-white'}`}>
+            {tab}
+          </button>
+        ))}
+      </div>
+
+
+				<main className='flex flex-col '>
 					<section className='flex grow w-full justify-center items-center pb-7 pt-3 border-b border-zinc-700'>
-						<div className='flex flex-col items-center gap-1'>
-							{/* <div className='flex justify-center items-center'><img src={noChats} alt="No Conversation" /></div> */}
-							<h2 className="text-[24px] font-roboto font-semibold mt-1 text-black dark:text-white">No friends yet</h2>
-							<p className="text-sm font-roboto text-black dark:text-zinc-400 max-w-75 text-center">Add friends and start connecting with people.</p>
-						</div>
+						{renderContent()}
 					</section>
 
 					<SuggestedFriends />
--- Chat-App/src/components/Contact/SuggestedFriends.jsx
+++ Chat-App/src/components/Contact/SuggestedFriends.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from "react";
-import { ref, onValue, getDatabase } from "firebase/database";
+import { ref, set, update, onValue, getDatabase } from "firebase/database";
 import app from "../../firebase/firebase.config";
 import { AuthContext } from "../../context/AuthProvider";
 import '../../index.css'
@@ -8,20 +8,10 @@ import { getRandomDarkColor } from '../../utility/getRandomDarkColor'
 import addFriend from '../../assets/add-friend.png'
 
 export default function SuggestedFriends(){
-	const darkColors = [
-		'bg-violet-700',
-		'bg-indigo-700',
-		'bg-blue-700',
-		'bg-emerald-700',
-		'bg-rose-700',
-		'bg-slate-700',
-		'bg-amber-700'
-	];
-	// Pili ng random class
-	const randomDarkBg = darkColors[Math.floor(Math.random() * darkColors.length)];
-
 	const { user } = useContext(AuthContext);
 	const [users, setUsers] = useState([]);
+	const [sentRequests, setSentRequests] = useState({});
+	const [laodingUid, setLoadingUid] = useState(null);
 	const db = getDatabase(app);
 
 	useEffect(() =>{
@@ -30,7 +20,6 @@ export default function SuggestedFriends(){
 		const unsubscribe = onValue(usersRef, (snapshot) =>{
 			if(snapshot.exists()){
 				const data = snapshot.val();
-
 				const usersArray = Object.entries(data).map(([uid, value]) => ({
 					uid,
 					...value
@@ -39,21 +28,68 @@ export default function SuggestedFriends(){
 			}
 		});
 
+		const requestRef = ref(db, `friendRequests/${user.uid}`);
+		const unsubscribeRequests = onValue(requestRef, (snapshot) => {
+			if(snapshot.exists()){
+				setSentRequests(snapshot.val());
+			}else{ setSentRequests({});}
+		});
+
 		// Pag nag exit si user sa page nag off ang pag get ng users
-		return () => unsubscribe();
-	}, []);
+		return () => {
+			unsubscribe();
+			unsubscribeRequests();
+		};
+
+	}, [user?.uid, db]);
+
 	const suggestedFriends = users.filter(u => u.uid !== user.uid);
+
+	/*
+	 * * Handles Sending / Canceling Friend Requests sa Firebase
+	 */
+	async function handleToggleFriendRequest(targetUid){
+		if (!user?.uid || !targetUid) return;
+		setLoadingUid(targetUid);
+
+		const requestRefSender = ref(db, `friendRequests/${user.uid}/${targetUid}`);
+		const requestRefReceiver = ref(db, `friendRequests/${targetUid}/${user.uidgetUid}`);
+
+		try {
+			const isAlreadySent = !!sentRequests[targetUid];
+			if (isAlreadySent) {
+				const updates = {};
+				updates[`friendRequests/${user.uid}/${targetUid}`] = null;
+				updates[`friendRequests/${targetUid}/${user.uid}`] = null;
+				await update(ref(db), updates);
+			} else {
+				// Kapag 'Add Friend' - I-save sa Firebase database
+				const timestamp = Date.now();
+				const updates = {};
+				updates[`friendRequests/${user.uid}/${targetUid}`] = { status: "sent", timestamp };
+				updates[`friendRequests/${targetUid}/${user.uid}`] = { status: "received", timestamp };
+				await update(ref(db), updates);
+			}
+		} catch (error) {
+			console.error("Error updating friend request:", error);
+		}finally{
+			setLoadingUid(null);
+		}
+	}
 
 
 	return(
 		<>
 			{suggestedFriends.length >= 1 ? (
-				<div className="flex flex-col justify-center mt-5 gap-y-3">
+				<div className="flex flex-col justify-center mt-5 gap-y-3 overflow-scroll scrollbar-none">
 					<div className="text-black dark:text-white font-semibold text-[16.5px]">
 						People you may know
 					</div>
 
 					{suggestedFriends.map((friend) =>{
+						const isRequested = !!sentRequests[friend?.uid];
+						const isLoading = laodingUid === friend?.uid;
+
 						return(
 							<div key={friend?.uid} className= 'flex gap-2.5 items-center grow h-12.5'>
 								<div className="image flex justify-center items-center text-white font-semibold w-10 h-10 rounded-full" style={{backgroundColor: friend?.avatarColor}}>
@@ -63,9 +99,15 @@ export default function SuggestedFriends(){
 									<p className="font-bold text-[14px] text-white">{`${friend?.firstName} ${friend?.lastName}`}</p>
 									<p className="text-[13px] text-gray-200 mb-1">{friend?.email}</p>
 								</div>
-								<button className="flex justify-center items-center px-3.5 py-2 gap-x-2 bg-zinc-100 rounded-lg  cursor-pointer">
-									<div className='flex justify-center items-center'><img className='w-5 h-5' src={addFriend} alt="Add Friend"/></div>
-									<p className="text-[14px] font-roboto font-semibold text-white dark:text-zinc-900">Add</p>
+								<button onClick={() => handleToggleFriendRequest(friend?.uid)} 
+								disabled={isLoading}
+								type="button"
+								className={`flex justify-center items-center px-3.5 py-2 gap-x-2 rounded-lg cursor-pointer ${
+                  isRequested ? 'bg-zinc-700 text-white': 'bg-zinc-100 dark:bg-zinc-100'}`}>
+									{/* <div className='flex justify-center items-center'><img className='w-5 h-5' src={addFriend} alt="Add Friend"/></div> */}
+									<p className={`text-[14px] font-roboto font-semibold ${isRequested ? 'text-white' : 'text-zinc-900' }`}>
+                    {isLoading ? 'Processing' : isRequested ? 'Cancel Request' : 'Add friend'}
+                  </p>
 								</button>
 							</div>
 						);
