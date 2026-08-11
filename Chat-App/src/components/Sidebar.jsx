0846f52f78c6688c99c5ee7596f80182713e6b07
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-08-11 19:02:56 +0800

    fix the profile settings when you click

--- Chat-App/src/components/Sidebar.jsx
+++ Chat-App/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
 import { getAuth, signOut } from "firebase/auth";
@@ -27,6 +27,9 @@ export default function Sidebar({isSidebarOpen, setIsSidebarOpen, isMobile}) {
 	const auth = getAuth(app);
 	const db = getDatabase(app);
 
+	const [isProfileOpen, setIsProfileOpen] = useState(false);
+	const profileRef = useRef(null);
+
 	const handleLogout = async () =>{
 		try {
       if (user?.uid) {
@@ -40,11 +43,24 @@ export default function Sidebar({isSidebarOpen, setIsSidebarOpen, isMobile}) {
     }
 	};
 
+	const handleToggleProfile = () => {
+  	setIsProfileOpen((prev) => !prev); // Kapag true, gagawing false. Kapag false, gagawing true!
+	};
 
-	function handleToggleProfile(){
-		console.log('open');
-	}
-	
+
+	useEffect(() => {
+    function handleClickOutside(event) {
+      // Kapag nag-click sa labas ng buong profileRef container -> ISARA
+      if (profileRef.current && !profileRef.current.contains(event.target)) {
+        setIsProfileOpen(false);
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
 	return(
 		<>
 			<aside className={`h-screen bg-zinc-900 text-white fixed top-0 left-0 px-3 py-4 flex flex-col font-roboto  ${isMobile ? 'hidden w-0' : isSidebarOpen ? "w-76" : "w-18"}`}>
@@ -105,8 +121,9 @@ export default function Sidebar({isSidebarOpen, setIsSidebarOpen, isMobile}) {
 					</div>
 
 					<div className={`w-full flex ${isSidebarOpen ? 'items-center gap-2' : 'flex-col items-center gap-1'}`}>
-
-						<div onClick={() => handleToggleProfile()}
+							
+						<button onClick={() => handleToggleProfile()}
+						ref={profileRef}
 						className={isSidebarOpen ? 'relative flex gap-2 items-center grow h-12.5 p-2 hover:bg-zinc-800 rounded-md cursor-pointer' : 'flex gap-2 items-center grow h-12.5 p-2rounded-md cursor-pointer'}>
 							<div className="image flex justify-center items-center w-9 h-9 rounded-full "
 							style={{ backgroundColor: getDarkColor(user?.uid) }}>
@@ -115,10 +132,10 @@ export default function Sidebar({isSidebarOpen, setIsSidebarOpen, isMobile}) {
 							{isSidebarOpen && (
 								<div className="flex flex-col justify-center">
 									<p className="font-bold text-[13px] lg:text-[14px]">{user?.displayName}</p>
-									<p className=" text-[12px] lg:text-[13px] text-gray-200 mb-1">{user?.email}</p>
+									<p className="text-[12px] pl-0.5 lg:text-[13px] text-gray-200 mb-1">{user?.email}</p>
 								</div>
 							)}
-						</div>
+						</button>
 						<button onClick={() => setIsSidebarOpen(!isSidebarOpen)}
 						className="w-9 h-9 flex justify-center items-center bg-zinc-700 cursor-pointer hover:bg-zinc-600 rounded-full">
 							<img className='w-5 h-5' src={sidebar} alt="Sidebar" />
@@ -126,34 +143,37 @@ export default function Sidebar({isSidebarOpen, setIsSidebarOpen, isMobile}) {
 					</div>
 				</div>
 
-				<div className={` absolute bottom-28 left-4.75 flex flex-col bg-[#242526] text-zinc-200 w-62 px-2 py-2 gap-y-2 rounded-xl shadow-2xl z-50 border border-zinc-700/40
-					/* Dito ginawa ang maliit na arrow tail sa bottom-left */
-					after:content-[''] 
-					after:absolute 
-					after:-bottom-2 
-					after:left-2 
-					after:w-0 
-					after:h-0 
-					after:border-l-8 after:border-l-transparent 
-					after:border-r-8  after:border-r-transparent 
-					after:border-t-8  after:border-t-[#242526]`}>
+				{isProfileOpen && (
+					<div className={`flex absolute ${isSidebarOpen ? 'bottom-20 left-27' : 'bottom-28 left-4.75'} flex-col bg-[#242526] text-zinc-200 w-62 px-2 py-2 gap-y-2 rounded-xl shadow-2xl z-50 border border-zinc-700/40
+						/* Dito ginawa ang maliit na arrow tail sa bottom-left */
+						after:content-[''] 
+						after:absolute 
+						after:-bottom-2 
+						after:left-2 
+						after:w-0 
+						after:h-0 
+						after:border-l-8 after:border-l-transparent 
+						after:border-r-8  after:border-r-transparent 
+						after:border-t-8  after:border-t-[#242526]`}>
 
-					<button className="flex items-center cursor-pointer border-b border-zinc-500">
-						<div className="flex gap-x-3 items-center w-full  hover:bg-zinc-600 hover:rounded-md px-2 py-2 mb-2">
-							<img className="shrink-0 w-5 h-5" src={settings} alt="Settings" />
-							<span className="text-black dark:text-zinc-200 font-semibold">Settings</span>
-						</div>
-					</button>
+						<button onClick={() => console.log('Settings')}
+						className="flex items-center cursor-pointer border-b border-zinc-700">
+							<div className="flex gap-x-3 items-center w-full  hover:bg-zinc-600 hover:rounded-md px-2 py-2 mb-2">
+								<img className="shrink-0 w-5 h-5" src={settings} alt="Settings" />
+								<span className="text-black dark:text-zinc-200 font-semibold">Settings</span>
+							</div>
+						</button>
 
-					<button onClick={() => handleLogout()} 
-					className="flex items-center cursor-pointer">
-						<div className="flex gap-x-3 items-center w-full  hover:bg-zinc-600 hover:rounded-md px-2 py-2 mb-2">
-							<img className="shrink-0 w-5 h-5" src={logout} alt="Settings" />
-							<span className="text-black dark:text-zinc-200 font-semibold text-[14px] sm:text-[15px]">Log out</span>
-						</div>
-					</button>
+						<button onClick={() => handleLogout()}
+						className="flex items-center cursor-pointer">
+							<div className="flex gap-x-3 pr-2 items-center w-full  hover:bg-zinc-600 hover:rounded-md px-2 py-2 mb-2">
+								<img className="shrink-0 w-5 h-5" src={logout} alt="Settings" />
+								<span className="text-black dark:text-zinc-200 font-semibold text-[14px] sm:text-[15px]">Log out</span>
+							</div>
+						</button>
 
-				</div>
+					</div>
+				)}
 				
 			</aside>
 		</>
